b4913162b373bd532b28d225b99b5616e456b5c4
unknown <you@wxample.com>
2024-12-20 09:34:13 +0530

    adding gopi files

---
+++ src/main/java/com/neoteric/gopi/gopi4.java
@@ -0,0 +1,22 @@
+package com.neoteric.gopi;
+
+public class gopi4 {
+    public static void main(String[]args){
+        int age1=    21;
+        int age2=  23  ;
+        if ((age1>18)&&(age2>20))
+        {
+            System.out.println("women are eligilbe for marriage");
+        } else if ((age1<18)&&(age2>21))
+        {
+            System.out.println("women are eligible for marriage");
+        } else if ((age1>17)&&(age2>21))
+        {
+            System.out.println("women are eligible for marriage");
+        }
+        else if((age1<17)&&(age2<20))
+        {
+            System.out.println("women are eligible for marriage");
+        }
+    }
+}
