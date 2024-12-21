0c6943a4f82b374c037522d44a7a9dbeb915c962
Gopi-490 <miriyalagopi31@gmail.com>
2024-12-21 13:00:20 +0530

    age

---
+++ src/main/java/Employee/Employee.java
@@ -0,0 +1,7 @@
+package Employee;
+
+public class Employee {
+     int sId;
+     String sName;
+     String Standed;
+}
---
+++ src/main/java/Employee/EmployeeTest.java
@@ -0,0 +1,16 @@
+package Employee;
+
+public class EmployeeTest {
+    public static void main(String[]args){
+        Employee e =new Employee();
+
+        e.sId=1;
+
+        e.sName= "gopi";
+
+        e.Standed= "10";
+
+        System.out.println("DETAILS" +    +e.sId + "              "    + e.sName     +"       "   +  e.Standed   );
+
+    }
+}
