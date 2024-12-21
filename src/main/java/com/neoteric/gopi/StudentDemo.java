f02749f6a396cb7f41ebfa7ef4fa88c0b67dc750
Gopi-490 <miriyalagopi31@gmail.com>
2024-12-21 08:41:23 +0530

    status commit

---
+++ src/main/java/com/neoteric/gopi/StudentDemo.java
@@ -0,0 +1,9 @@
+package com.neoteric.gopi;
+
+public class StudentDemo {
+    public int sId;
+    public String sName;
+    public String standard;
+
+
+}
---
+++ src/main/java/com/neoteric/gopi/StudentTest.java
@@ -0,0 +1,15 @@
+package com.neoteric.gopi;
+
+public class StudentTest {
+    public static void main(String[] args) {
+        StudentDemo studentDemo=new StudentDemo();
+
+        studentDemo.sId=1;
+
+        studentDemo.sName="Gopi";
+
+        studentDemo.standard="10";
+
+        System.out.println(studentDemo.sName);
+    }
+}
